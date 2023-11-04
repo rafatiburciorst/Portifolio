7aa58e806282f2f91db0bd923b6e7134192d09a7
rafatiburciorst <rafael.tiburcio@live.com>
2023-11-04 12:00:42 -0300

    github actions

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
 
+const port = Number(process.env.PORT) || 3000
+
 app.listen({
     host: '0.0.0.0',
-    port: 3333
+    port: port
 }).then(() => console.log('Server is running on port 3333'))
